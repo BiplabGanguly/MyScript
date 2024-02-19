a7bc242920274fec80c184cdda3c7405f5288d70
biplabganguly <biplab123.abcd@gmail.com>
2024-02-19 19:10:40 +0530

    add all

---
+++ myscript/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render,redirect
+import openai
+
+openai.api_key = "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"
+
+
+def home_page(req):
+    return render(req,'index.html')
+
+def script_page(req):
+    return render(req,'myscript.html')
+
+def post_script_page(req):
+    if req.method == "POST":
+        title = req.POST['title']
+        language = req.POST['language']
+        synopsis = req.POST['synopsis']
+        genre = req.POST['genre']
+        plot = req.POST['plot']
+
+        script = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [
+                    {"role" : "user" , "content" : "create a full script" },
+                    {"role" : "user" , "content" :  f"title: {title}"},
+                    {"role" : "user" , "content" :  f"language of the script: {language}"},
+                    {"role" : "user" , "content" : f"synopsis : {synopsis}" },
+                    {"role" : "user" , "content" :  f"gonre : {genre}"},
+                    {"role" : "user" , "content" :  f"plot : {plot}"}
+            ]
+        )
+        data = {'content' : script['choices'][0]['message']['content'].strip()}
+        return render(req,'myscript.html',data)
+    
+    return redirect('index')
